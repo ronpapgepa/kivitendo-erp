eda89913a3456625769d615d435e7729c152d9ac
Werner Hahn <wh@futureworldsearch.net>
2016-04-19 20:00:17 +0200

    Shopmodul: Kategorien werden in die DB geschrieben
    Conflicts:
    	SL/DB/MetaSetup/ShopPart.pm
    
    Shoppart: Shopkategorien sind zugeordnet und werden mit checkbox angezeigt
    
    Kategorien werden mit ID und Name in die DB geschrieben. Ein Komma im Name erzeugt noch Fehler. Der Pfad muss noch gemacht werden
    
    Conflicts:
    	SL/ShopConnector/Shopware.pm
    
    ShopPart: ArtikelKategorien werden über ID in den Shop dem Artikle zugeordnet

--- SL/Controller/ShopPart.pm
+++ SL/Controller/ShopPart.pm
@@ -56,31 +56,22 @@ sub action_update_shop {
 
 sub action_show_files {
   my ($self) = @_;
-$main::lxdebug->dump(0, 'WH: Show_Files',\$::form);
+
   require SL::DB::File;
   my $images = SL::DB::Manager::File->get_all_sorted( where => [ trans_id => $::form->{id}, modul => $::form->{modul}, file_content_type => { like => 'image/%' } ], sort_by => 'position' );
-  $main::lxdebug->dump(0, 'WH: ',\$images);
-  #my $html = $self->render('shop_part/_list_images', { output => 0 }, IMAGES => $images);
+
   $self->render('shop_part/_list_images', { header => 0 }, IMAGES => $images);
-  #$main::lxdebug->dump(0, 'WH: ',\$html);
-
-  #$self->js->html('#shop_images', $html);
-  #$self->js->render;
-
-  # $self->render('customer_vendor_turnover/count_open_items_by_year', { layout => 0 });
-
 
 }
 
 sub action_get_categories {
   my ($self) = @_;
 
-  my $shop_part = SL::DB::Manager::ShopPart->find_by(id => $::form->{shop_part_id});
-  die unless $shop_part;
+#  my $shop_part = SL::DB::Manager::ShopPart->find_by(id => $::form->{shop_part_id});
+#  die unless $shop_part;
   require SL::Shop;
-  my $shop = SL::Shop->new( config => $shop_part->shop );
+  my $shop = SL::Shop->new( config => $self->shop_part->shop );
   my $categories = $shop->connector->get_categories;
-  $main::lxdebug->dump(0, 'WH: GET',\$categories);
 
   $self->js
     ->run(
@@ -145,21 +136,35 @@ sub render_shop_part_edit_dialog {
   $self->js->render;
 }
 
-sub render_shop_categories_edit_dialog {
+sub action_save_categories {
   my ($self) = @_;
 
-  # when self->shop_part is called in template, it will be an existing shop_part with id,
-  # or a new shop_part with only part_id and shop_id set
-  $self->js
-    ->run(
-      'kivi.shop_part.shop_part_dialog',
-      t8('Shopcategories'),
-      $self->render('shop_part/categories', { output => 0 }) #, shop_part => $self->shop_part)
-    )
-    ->reinit_widgets;
+  my @categories =  @{ $::form->{categories} || [] };
+  $main::lxdebug->dump(0, 'WH: KATEGORIEN: ', \@categories);
+  my @cat = ();
+  foreach my $cat ( @categories) {
+    # TODO das koma macht Probleme z.B kategorie "Feldsalat, Rapunzel"
+    my @temp = [split(/,/,$cat)];
+    push( @cat, @temp );
+  }
+  $main::lxdebug->dump(0, 'WH: KAT2:',\@cat);
 
-  $self->js->render;
+  my $categories->{shop_category} = \@cat;
+
+  my $params = delete($::form->{shop_part}) || { };
+
+  $self->shop_part->assign_attributes(%{ $params });
+  $self->shop_part->assign_attributes(%{ $categories });
+
+  $self->shop_part->save;
+
+  flash('info', t8('The categories has been saved.'));
+
+  $self->js->run('kivi.shop_part.close_dialog')
+           ->flash('info', t8("Updated categories"))
+           ->render;
 }
+
 sub action_reorder {
   my ($self) = @_;
 $main::lxdebug->message(0, "WH:REORDER ");
