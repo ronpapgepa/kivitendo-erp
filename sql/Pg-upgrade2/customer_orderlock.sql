6a9e40657bc40fcf858cd8277fafda4579fa5e10
Werner Hahn <wh@futureworldsearch.net>
2016-10-20 16:44:25 +0200

    Shopmodul: sql-Updatesfiles Fehlerbehebung

--- sql/Pg-upgrade2/customer_orderlock.sql
+++ sql/Pg-upgrade2/customer_orderlock.sql
@@ -2,4 +2,4 @@
 -- @description: Boolean Auftragssperre benötigt bei shoporders
 -- @depends: release_3_4_1 shops
 -- @ignore: 0
-ALTER TABLE customer ADD COLUMN order_lock boolean default 0;
+ALTER TABLE customer ADD COLUMN order_lock boolean default 'f';
