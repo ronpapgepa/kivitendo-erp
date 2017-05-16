64c12df64c6ee056bd7571bf751e6fd0d1e60766
Werner Hahn <wh@futureworldsearch.net>
2017-05-16 08:56:44 +0200

    Shopmodul: Bilder hochladen

---
+++ SL/DB/ShopImage.pm
@@ -0,0 +1,13 @@
+# This file has been auto-generated only because it didn't exist.
+# Feel free to modify it at will; it will not be overwritten automatically.
+
+package SL::DB::ShopImage;
+
+use strict;
+
+use SL::DB::MetaSetup::ShopImage;
+use SL::DB::Manager::ShopImage;
+
+__PACKAGE__->meta->initialize;
+
+1;
